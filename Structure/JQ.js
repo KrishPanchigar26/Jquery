4dcf29b51e0ae3bb0528decb3147983cca8752fb
KrishPanchigar26 <151987264+KrishPanchigar26@users.noreply.github.com>
2023-12-14 12:21:24 +0530

    Add files via upload

---
+++ Structure/JQ.js
@@ -0,0 +1,20 @@
+// function func1(){
+//     $("#img").fadeToggle(3000);
+//     console.log("hello");
+// }
+
+function func1(){
+
+    // $('element name').action() //syntax
+
+    // $("div").css("background-color","orange")
+    // $(".para").css("font-style", "italic")
+
+    // $('#div1,#div2').css('background-color','green')
+    // $("#div1, li").css('background-color','purple')
+    // $("div > p").fadeToggle();
+    // $("div  p").fadeToggle();
+    // $("p:first").fadeToggle() // it will only for first para,id,class
+    $("li:odd").fadeToggle()// also we can it for even
+    
+}
